8c9ac442224a60af4812093f93432996d2415243
u19069716 <u19069716@tuks.co.za>
2021-10-12 14:13:59 +0200

    Implemented the menu button & the menu

---
+++ js/main.js
@@ -0,0 +1,30 @@
+window.onload = () => {
+  console.log("Javascript loaded");
+
+  // Collect interactive HTML elements
+  const menuButton = document.getElementById("menu-button");
+  const menu = document.getElementById("menu");
+  const homeNavLink = document.getElementById("home-navlink");
+  const projectsNavLink = document.getElementById("projects-navlink");
+  const merchandiseNavLink = document.getElementById("merchandise-navlink");
+  const contactUsNavLink = document.getElementById("contact-us-navlink");
+
+  // Add event listener to menu button to display menu when clicked
+  menuButton.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+  });
+
+  // Add event listener to navlink items to navigate to the relevant urls when clicked
+  homeNavLink.addEventListener("click", () => {
+    window.location.href = "index.html";
+  });
+  projectsNavLink.addEventListener("click", () => {
+    window.location.href = "projects.html";
+  });
+  merchandiseNavLink.addEventListener("click", () => {
+    window.location.href = "store.html";
+  });
+  contactUsNavLink.addEventListener("click", () => {
+    window.location.href = "AboutUs.html";
+  });
+};
