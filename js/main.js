19acd6b3e77a9ba365caa230a2d5d3186216ce22
u19069716 <u19069716@tuks.co.za>
2021-10-14 18:24:54 +0200

    Added javascript for projects page interactivity

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+///////////////
+// Functions //
+///////////////
+
+function removeCSSClassFromHTMLElementCollection(elementCollection, cssClass) {
+  for (let index = 0; index < elementCollection.length; index++) {
+    const element = elementCollection[index];
+    element.classList.remove(cssClass);
+  }
+}
+
+function setSelectedProject(projectIndex) {
+  const targetProject = document.getElementById(
+    "project-" + (projectIndex + 1)
+  );
+  targetProject.classList.add("project-selected");
+}
+
+/////////////////////
+// Main Entrypoint //
+/////////////////////
+
 window.onload = () => {
   console.log("Javascript loaded");
 
@@ -8,6 +30,8 @@ window.onload = () => {
   const projectsNavLink = document.getElementById("projects-navlink");
   const merchandiseNavLink = document.getElementById("merchandise-navlink");
   const contactUsNavLink = document.getElementById("contact-us-navlink");
+  const projectsNavItems = document.getElementsByClassName("projects-nav-item");
+  const projects = document.getElementsByClassName("project");
 
   // Add event listener to menu button to display menu when clicked
   menuButton.addEventListener("click", () => {
@@ -27,4 +51,26 @@ window.onload = () => {
   contactUsNavLink.addEventListener("click", () => {
     window.location.href = "AboutUs.html";
   });
+
+  // Set selected project to first project
+  setSelectedProject(0);
+  projectsNavItems[0].classList.add("projects-nav-item-selected");
+
+  // Iterate over all projects nav items & add event listener to allow selection of nav items
+  for (let index = 0; index < projectsNavItems.length; index++) {
+    const projectNavItem = projectsNavItems[index];
+    projectNavItem.addEventListener("click", (event) => {
+      removeCSSClassFromHTMLElementCollection(
+        projectsNavItems,
+        "projects-nav-item-selected"
+      );
+      removeCSSClassFromHTMLElementCollection(projects, "project-selected");
+      setSelectedProject(index);
+      if (event.target.tagName === "LI") {
+        event.target.classList.add("projects-nav-item-selected");
+      } else {
+        event.target.parentElement.classList.add("projects-nav-item-selected");
+      }
+    });
+  }
 };
